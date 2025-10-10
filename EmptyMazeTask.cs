9241f3800f40aa2dab190320edee696ea5de907b
fradere8 <kulikovaos2845@gmail.com>
2025-10-11 00:02:10 +0500

    Add files via upload

---
+++ EmptyMazeTask.cs
@@ -0,0 +1,23 @@
+﻿namespace Mazes;
+
+public static class EmptyMazeTask
+{
+	public static void MoveOut(Robot robot, int width, int height)
+	{
+		void MovingRight(int width)
+		{
+			for (int i = 0; i < width; i++)
+			{
+				robot.MoveTo(Direction.Right);
+			}
+		}
+
+		void MovingDown(int height)
+		{
+			for (int i = 0; i < width; i++)
+			{
+				robot.MoveTo(Direction.Right);
+			}
+		}
+	}
+}
