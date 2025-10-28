4fe40e73517fba090006d85f715682301181fe79
fradere8 <kulikovaos2845@gmail.com>
2025-10-28 21:57:31 +0500

    Update SobelFilterTask.cs

--- SobelFilterTask.cs
+++ SobelFilterTask.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Drawing;
 using Avalonia.Controls.Platform;
 using Tmds.DBus.Protocol;
@@ -11,39 +11,39 @@ internal static class SobelFilterTask
         var width = g.GetLength(0);
         var height = g.GetLength(1);
         var result = new double[width, height];
-        var radius = (sx.GetLength(0) - 1) / 2;
-        var sy = TransMatrix(sx);
-        for (int x = radius; x < width - radius; x++)
-            for (int y = radius; y < height - radius; y++)
+        var radius = (sx.GetLength(0)) / 2;
+        var sy = ToTransMatrix(sx);
+        for (var x = radius; x < width - radius; x++)
+            for (var y = radius; y < height - radius; y++)
             {
                 var gx = MultiplyMatrix(g, x, y, sx, radius);
                 var gy = MultiplyMatrix(g, x, y, sy, radius);
                 result[x,y] = Math.Sqrt(gx * gx + gy * gy);
             }
         return result;
-    }
+	}
+
     private static double MultiplyMatrix(double[,] g, int xCenter, int yCenter, double[,] sx, int radius)
     {
-        var widthSx = sx.GetLength(0);
-        var heightSx = sx.GetLength(1);
         var m = 0.0;
-        for (var i = -radius; i < radius; i++)
+        for (var i = -radius; i <= radius; i++)
         {
-            for (var j = -radius; j < radius; j++)
+            for (var j = -radius; j <= radius; j++)
             {
-                m += g[xCenter+i,yCenter+i]*sx[i,j];
+                m += g[xCenter + i,yCenter + j]*sx[i + radius,j + radius];
             }
         }
         return m;
-    }
-    private static double[,] TransMatrix(double[,] sx)
+	}
+
+    private static double[,] ToTransMatrix(double[,] sx)
     {
         var widthSx = sx.GetLength(0);
         var heightSx = sx.GetLength(1);
         var sy = new double[widthSx,heightSx];
-        for (var y = 0; y < heightSx; y++)
+        for (var x = 0; x < heightSx; x++)
         {
-            for (var x = 0; x < widthSx; x++)
+            for (var y = 0; y < widthSx; y++)
             {
                 sy[x, y] = sx[y, x];
             }
@@ -51,3 +51,5 @@ internal static class SobelFilterTask
         return sy;
     }
 }
+
+
