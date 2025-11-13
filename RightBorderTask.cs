f6d77c6eb9395e5a7bd31aba405af0333635d18f
fradere8 <kulikovaos2845@gmail.com>
2025-11-13 20:12:26 +0500

    Add files via upload

---
+++ RightBorderTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Autocomplete;
+
+public class RightBorderTask
+{
+	public static int GetRightBorderIndex(IReadOnlyList<string> phrases, string prefix, int left, int right)
+	{
+		while (left + 1 < right)
+		{
+			var middle = (right + left) / 2;
+			if (string.Compare(phrases[middle], prefix, StringComparison.InvariantCultureIgnoreCase) > 0
+							&& !phrases[middle].StartsWith(prefix, StringComparison.InvariantCultureIgnoreCase))
+			{
+				right = middle;
+			}
+			else
+			{
+				left = middle;
+			}
+		}
+		
+		return right;
+	}
+}
